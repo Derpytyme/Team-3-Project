b863c3fa8d4d3943c8c91ffe97dfdbd1f97f5a1c
Benjamin6282 <benjamin.cobb@maine.edu>
2024-11-12 17:35:08 -0500

    added connection string and started constructor for database class

--- PokemonApp/src/main/java/com/app/database/Database.java
+++ PokemonApp/src/main/java/com/app/database/Database.java
@@ -1,1 +1,25 @@
 package main.java.com.app.database;
+
+import org.bson.Document;
+
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoClients;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
+
+import static com.mongodb.client.model.Filters.*;
+
+public class Database {
+    private String connectionString = "mongodb+srv://benjamincobb:WGfcZDJ18Lm3n5CF@cos225cluster.ztzml.mongodb.net/?retryWrites=true&w=majority&appName=COS225Cluster";
+
+    public Database(){
+        try (MongoClient mongoclient = MongoClients.create(connectionString)) {
+
+
+            
+        }
+
+    }
+
+    
+}
