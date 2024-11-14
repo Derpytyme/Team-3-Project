1d0cbb56eb6f9f0364171f6a67fc53448dba0bf6
l-anderson-e <157403432+l-anderson-e@users.noreply.github.com>
2024-11-13 20:22:56 -0500

    Update Menu.java
    just outlining it, not super sure what I need to be doing here

--- PokemonApp/src/main/java/com/app/menu/Menu.java
+++ PokemonApp/src/main/java/com/app/menu/Menu.java
@@ -1,5 +1,19 @@
 package main.java.com.app.menu;
+import org.bson.Document;
+import com.movieapp.database.Database;
+import com.movieapp.movie.Movie;
+import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 
 public class Menu {
+
+//method that will read a csv file and put the collection into the database
+public void start(){
+
+}
+
+
     
 }
