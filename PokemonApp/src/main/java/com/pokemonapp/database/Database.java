93482142fbcf988833afdc6a2039e96b17ea7fa6
Benjamin6282 <benjamin.cobb@maine.edu>
2024-11-14 15:33:51 -0500

    added constructor and create collection method to database

--- PokemonApp/src/main/java/com/pokemonapp/database/Database.java
+++ PokemonApp/src/main/java/com/pokemonapp/database/Database.java
@@ -3,15 +3,10 @@ package com.pokemonapp.database;
 import org.bson.Document;
 
 import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoClients;
 import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoDatabase;
-import com.mongodb.client.*;
-import java.util.List;
-import java.util.ArrayList;
 
-
-
-import static com.mongodb.client.model.Filters.*;
 
 public class Database {
     private String connectionString, databaseName, collectionName;
@@ -25,34 +20,37 @@ public class Database {
         this.connectionString = "mongodb+srv://benjamincobb:WGfcZDJ18Lm3n5CF@cos225cluster.ztzml.mongodb.net/?retryWrites=true&w=majority&appName=COS225Cluster";
         this.databaseName = dbName;
         this.collectionName = collectionName;
-
-
-        // Initialize MongoDB connection
-        mongoClient = MongoClients.create(connectionString);
-        database = mongoClient.getDatabase(databaseName);
-        collection = database.getCollection(collectionName);
     }
 
-    // Method to search for documents based on user input
-    public List<Document> searchByDescription(String userInput) {
-        List<Document> results = new ArrayList<>();
-        results = collection.find(text(userInput)).into(new ArrayList<>());
+    public Database(String connectionString, String dbName, String collectionName) {
+        this.connectionString = connectionString;
+        this.databaseName = dbName;
+        this.collectionName = collectionName;
 
-        return results;
     }
 
-    // Method to handle search results
-    public List<String> getSearchResults(String userInput) {
-        List<Document> results = searchByDescription(userInput);
-    
-        List<String> resultStrings = new ArrayList<>();
-    
-        return resultStrings;
+    public void addToDatabase(Document document) {
+
+        try (MongoClient mongoClient = MongoClients.create(connectionString)) {
+
+            MongoDatabase pokemonDatabase = mongoClient.getDatabase(this.databaseName);
+            MongoCollection<Document> pokemonCollection = pokemonDatabase.getCollection(this.collectionName);
+
+            pokemonCollection.insertOne(document);
+
+        }
+
     }
 
-    // Method to close database connection
-    public void close() {
-        mongoClient.close();
+    public void createCollection() {
+
+        try (MongoClient mongoClient = MongoClients.create(connectionString)) {
+
+            MongoDatabase pokemonDatabase = mongoClient.getDatabase(this.databaseName);
+            pokemonDatabase.createCollection(this.collectionName);
+
+        }
+
     }
 
     }
