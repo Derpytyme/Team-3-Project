e2d830475f64d05e57288debff1f4739570a8d60
l-anderson-e <157403432+l-anderson-e@users.noreply.github.com>
2024-11-13 20:08:20 -0500

    Update Pokemon.java
    to include getters and to append to document

--- PokemonApp/src/main/java/com/app/pokemon/Pokemon.java
+++ PokemonApp/src/main/java/com/app/pokemon/Pokemon.java
@@ -1,5 +1,30 @@
 package main.java.com.app.pokemon;
 
+import org.bson.Document;
+import java.util.ArrayList;
+
 public class Pokemon {
-    
+    String name;
+    ArrayList<String> types;
+    String caption;
+
+    public String getName() {
+        return name;
+    }
+
+    public ArrayList<String> getTypes() {
+        return types;
+    }
+
+    public String getCaption() {
+        return caption;
+    }
+
+    public Document getDocument() {
+        Document document = new Document();
+        document.append("name", name);
+        document.append("types", types);
+        document.append("caption", caption);
+        return document;
+    }
 }
