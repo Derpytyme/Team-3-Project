ecf9933574bba10e0ce935caf591d56d8db83e3c
Benjamin6282 <benjamin.cobb@maine.edu>
2024-12-06 22:04:52 -0500

    added skeleton code of Pokémon classifier

--- PokemonApp/src/main/java/com/pokemonapp/nlp/PokemonClassifier.java
+++ PokemonApp/src/main/java/com/pokemonapp/nlp/PokemonClassifier.java
@@ -10,5 +10,20 @@ import com.pokemonapp.pokemon.Pokemon;
 public class PokemonClassifier {
     private Processor processor;
     private HashSet<String> vocabulary = new HashSet<>();
+
+    public PokemonClassifier(Processor processor) {
+        this.processor = processor;
+    }
     
+    public void addSample(BsonValue id, String description) {
+        //needs implimentation
+    }
+
+
+
+    public String classify(String description) {
+        //needs implimentation
+        String clasification = " ";
+        return clasification;
+    }
 }
