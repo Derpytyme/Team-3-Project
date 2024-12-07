62f201b4dfcf99a92fed39b83c9c2f1205b89701
Benjamin6282 <benjamin.cobb@maine.edu>
2024-12-06 19:41:47 -0500

    Added load stop words

--- PokemonApp/src/main/java/com/pokemonapp/nlp/Processor.java
+++ PokemonApp/src/main/java/com/pokemonapp/nlp/Processor.java
@@ -7,5 +7,23 @@ import java.util.HashSet;
 import java.util.Set;
 
 public class Processor {
+    private Set<String> stopWords;
+
+        public Processor(String stopWordsFilePath) {
+        stopWords = new HashSet<>();
+        loadStopWords(stopWordsFilePath);
+        }
+
+        private void loadStopWords(String stopWordsFilePath) {
+            try (BufferedReader br = new BufferedReader(new FileReader(stopWordsFilePath))) {
+                String line;
+                while ((line = br.readLine()) != null) {
+                    stopWords.add(line.trim());
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
     
 }
