f7fa9ba2b706f1e7a248e83643260250d55d6d2a
Benjamin6282 <benjamin.cobb@maine.edu>
2024-12-06 19:39:15 -0500

    Added import statements to Pokemon classifiier

--- PokemonApp/src/main/java/com/pokemonapp/nlp/PokemonClassifier.java
+++ PokemonApp/src/main/java/com/pokemonapp/nlp/PokemonClassifier.java
@@ -1,5 +1,14 @@
 package com.pokemonapp.nlp;
 
+import java.util.HashMap;
+import java.util.HashSet;
+
+import org.bson.BsonValue;
+
+import com.pokemonapp.pokemon.Pokemon;
+
 public class PokemonClassifier {
+    private Processor processor;
+    private HashSet<String> vocabulary = new HashSet<>();
     
 }
