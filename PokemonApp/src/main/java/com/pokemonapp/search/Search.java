9390a0e14e40f902c10eba2b1f37421570307e91
Benjamin6282 <benjamin.cobb@maine.edu>
2024-12-09 13:13:40 -0500

    Finished search will need debugging

--- PokemonApp/src/main/java/com/pokemonapp/search/Search.java
+++ PokemonApp/src/main/java/com/pokemonapp/search/Search.java
@@ -20,29 +20,59 @@ public class Search {
     private int SearchValue;
     Database database = new Database("pokemon_app_databse", "pokemon_data"); 
     ArrayList<Document> data = new ArrayList<Document>();
+
+    ArrayList<Pokemon> PokemonList = new ArrayList<Pokemon>();
     
     
     public Search(int SearchValue){
         this.SearchValue = SearchValue;
         data = database.getAllDocuments();
-        ArrayList<Integer> HP = new ArrayList<Integer>();
+     
+
 
         for (int i = 0; i<data.size(); i++){
             Pokemon pokemonObject = new Pokemon(data.get(i));
-            HP.add(pokemonObject.GetHp());
-
+            PokemonList.add(pokemonObject);
         }
+
+
+
     }
     public void HPSearch(){
-        
+        int numOfResults = 0;
+        ArrayList<Pokemon> PokemonResultList = new ArrayList<Pokemon>();
+        PokemonList.sort( (a, b) -> { return Integer.compare((a.GetHp()), (b.GetHp())); } );
+        for (int i = 0; i< PokemonList.size() && numOfResults <5; i++){
+            if (PokemonList.get(i).GetHp() > SearchValue){
+                PokemonResultList.add(PokemonList.get(i));
+            }
+        }
+        System.out.println(PokemonResultList);
     }
 
     public void AttackSearch(){
+        int numOfResults = 0;
+        ArrayList<Pokemon> PokemonResultList = new ArrayList<Pokemon>();
+        PokemonList.sort( (a, b) -> { return Integer.compare((a.GetAttack()), (b.GetAttack())); } );
+        for (int i = 0; i< PokemonList.size() && numOfResults <5; i++){
+            if (PokemonList.get(i).GetAttack() > SearchValue){
+                PokemonResultList.add(PokemonList.get(i));
+            }
+        }
+        System.out.println(PokemonResultList);
 
     }
 
     public void DefenseSearch(){
-
+        int numOfResults = 0;
+        ArrayList<Pokemon> PokemonResultList = new ArrayList<Pokemon>();
+        PokemonList.sort( (a, b) -> { return Integer.compare((a.Getdefense()), (b.Getdefense())); } );
+        for (int i = 0; i< PokemonList.size() && numOfResults <5; i++){
+            if (PokemonList.get(i).Getdefense() > SearchValue){
+                PokemonResultList.add(PokemonList.get(i));
+            }
+        }
+        System.out.println(PokemonResultList);
     }
 
     
