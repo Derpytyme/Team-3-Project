4ac7acba9659e3e22eb9ae08ae3da132298c4393
Benjamin6282 <benjamin.cobb@maine.edu>
2024-12-09 09:49:59 -0500

    Update Search.java

--- PokemonApp/src/main/java/com/pokemonapp/search/Search.java
+++ PokemonApp/src/main/java/com/pokemonapp/search/Search.java
@@ -13,15 +13,25 @@ import com.mongodb.client.MongoClients;
 import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoDatabase;
 import com.pokemonapp.database.Database;
+import com.pokemonapp.pokemon.Pokemon;
 
 
 public class Search {
     private int SearchValue;
     Database database = new Database("pokemon_app_databse", "pokemon_data"); 
     ArrayList<Document> data = new ArrayList<Document>();
-    data = database.getAllDocuments();
+    
+    
     public Search(int SearchValue){
         this.SearchValue = SearchValue;
+        data = database.getAllDocuments();
+        ArrayList<Integer> HP = new ArrayList<Integer>();
+
+        for (int i = 0; i<data.size(); i++){
+            Pokemon pokemonObject = new Pokemon(data.get(i));
+            HP.add(pokemonObject.GetHp());
+
+        }
     }
     public void HPSearch(){
         
