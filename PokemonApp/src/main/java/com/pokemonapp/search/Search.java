76b7beff6d82b664a01f910f6728f61b109b76f2
Benjamin6282 <benjamin.cobb@maine.edu>
2024-12-09 09:22:49 -0500

    Continued implementing search class

--- PokemonApp/src/main/java/com/pokemonapp/search/Search.java
+++ PokemonApp/src/main/java/com/pokemonapp/search/Search.java
@@ -1,8 +1,25 @@
 package com.pokemonapp.search;
 
+import java.util.ArrayList;
+import java.io.BufferedReader;
+import java.io.FileReader;
+
+
+import org.bson.BsonValue;
+import org.bson.Document;
+
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoClients;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
+import com.pokemonapp.database.Database;
+
+
 public class Search {
     private int SearchValue;
-
+    Database database = new Database("pokemon_app_databse", "pokemon_data"); 
+    ArrayList<Document> data = new ArrayList<Document>();
+    data = database.getAllDocuments();
     public Search(int SearchValue){
         this.SearchValue = SearchValue;
     }
@@ -18,4 +35,5 @@ public class Search {
 
     }
 
+    
 }
